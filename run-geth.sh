4795b284f329255856511624db87665ab9e3b7f6
Chiano <bigchiano@protonmail.com>
2023-07-20 21:58:07 +0300

    Update run-geth.sh

--- run-geth.sh
+++ run-geth.sh
@@ -2,7 +2,6 @@
 
 DELAY_SEC=15
 MINNER_ADDR=0x3b8a98990826Cf43c555014D7dE850cD98aC9bcF
-SERVER_IP=
 CHAIN_ID=10201
 DATA_DIR=./data
 IS_ARCHIVE_NODE=true
@@ -19,13 +18,13 @@ runWithDelay $DELAY_SEC ./build/bin/geth attach --exec admin.nodeInfo.enode $DAT
 
 if $IS_ARCHIVE_NODE
 then
-    nohup ./build/bin/geth --datadir $DATA_DIR --networkid $CHAIN_ID --http --http.addr 0.0.0.0 --port 30303 --http.port 8545 --http.api personal,eth,net,debug,txpool,web3,admin --http.corsdomain '*' --ws --ws.addr 0.0.0.0 --ws.port 8546 --ws.api personal,eth,net,debug,txpool --http.vhosts '*' --syncmode full --gcmode archive --nat extip:$SERVER_IP --mine --miner.threads=1 --miner.etherbase=$MINNER_ADDR &
+    nohup ./build/bin/geth --datadir $DATA_DIR --networkid $CHAIN_ID --http --http.addr 0.0.0.0 --port 30303 --http.port 8545 --http.api personal,eth,net,debug,txpool,web3,admin --http.corsdomain '*' --ws --ws.addr 0.0.0.0 --ws.port 8546 --ws.api personal,eth,net,debug,txpool --http.vhosts '*' --syncmode full --gcmode archive --mine --miner.threads=1 --miner.etherbase=$MINNER_ADDR &
 elif $IS_LIGHT_NODE
 then
-    nohup ./build/bin/geth --datadir $DATA_DIR --networkid $CHAIN_ID --http --http.addr 0.0.0.0 --port 30303 --http.port 8545 --http.api personal,eth,net,debug,txpool,web3,admin --http.corsdomain '*' --ws --ws.addr 0.0.0.0 --ws.port 8546 --ws.api personal,eth,net,debug,txpool --http.vhosts '*' --syncmode snap --nat extip:$SERVER_IP --mine --miner.threads=1 --miner.etherbase=$MINNER_ADDR &
+    nohup ./build/bin/geth --datadir $DATA_DIR --networkid $CHAIN_ID --http --http.addr 0.0.0.0 --port 30303 --http.port 8545 --http.api personal,eth,net,debug,txpool,web3,admin --http.corsdomain '*' --ws --ws.addr 0.0.0.0 --ws.port 8546 --ws.api personal,eth,net,debug,txpool --http.vhosts '*' --syncmode snap --mine --miner.threads=1 --miner.etherbase=$MINNER_ADDR &
 elif $IS_FULL_NODE
 then
-    nohup ./build/bin/geth --datadir $DATA_DIR --networkid $CHAIN_ID --http --http.addr 0.0.0.0 --port 30303 --http.port 8545 --http.api personal,eth,net,debug,txpool,web3,admin --http.corsdomain '*' --ws --ws.addr 0.0.0.0 --ws.port 8546 --ws.api personal,eth,net,debug,txpool --http.vhosts '*' --syncmode full --nat extip:$SERVER_IP --mine --miner.threads=1 --miner.etherbase=$MINNER_ADDR &
+    nohup ./build/bin/geth --datadir $DATA_DIR --networkid $CHAIN_ID --http --http.addr 0.0.0.0 --port 30303 --http.port 8545 --http.api personal,eth,net,debug,txpool,web3,admin --http.corsdomain '*' --ws --ws.addr 0.0.0.0 --ws.port 8546 --ws.api personal,eth,net,debug,txpool --http.vhosts '*' --syncmode full --mine --miner.threads=1 --miner.etherbase=$MINNER_ADDR &
 else
     echo "Nothing...";
 fi
